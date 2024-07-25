4c08be5d1f2563040d956811635608accd3041cd
Nazmi Şimşek <nazmisimsekcom@gmail.com>
2024-07-25 23:35:38 +0300

    Ekleme

--- app/src/main/java/com/example/passwordmanagementapp/view/MainActivity.kt
+++ app/src/main/java/com/example/passwordmanagementapp/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.passwordmanagementapp
+package com.example.passwordmanagementapp.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.passwordmanagementapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
