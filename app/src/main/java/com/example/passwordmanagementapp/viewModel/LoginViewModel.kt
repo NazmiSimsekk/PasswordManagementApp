f60719036e289d4e7cd2c2c0f92b89c44673a12b
Nazmi Şimşek <nazmisimsekcom@gmail.com>
2024-07-27 16:00:04 +0300

    Ekleme

--- app/src/main/java/com/example/passwordmanagementapp/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/example/passwordmanagementapp/viewModel/LoginViewModel.kt
@@ -1,12 +1,5 @@
 package com.example.passwordmanagementapp.viewModel
 
-import android.view.View
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.navigation.Navigation
-import com.example.passwordmanagementapp.util.SignupClick
-import com.example.passwordmanagementapp.view.LoginFragmentDirections
 
-class LoginViewModel: ViewModel() {
-
-}
+class LoginViewModel: ViewModel()
