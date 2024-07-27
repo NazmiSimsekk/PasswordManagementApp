c7432f9da125382441269801d9d9e1aa7b79980b
Nazmi Şimşek <nazmisimsekcom@gmail.com>
2024-07-27 16:01:58 +0300

    Ekleme

--- app/src/main/java/com/example/passwordmanagementapp/view/LoginFragment.kt
+++ app/src/main/java/com/example/passwordmanagementapp/view/LoginFragment.kt
@@ -1,33 +1,25 @@
 package com.example.passwordmanagementapp.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.passwordmanagementapp.R
 import com.example.passwordmanagementapp.databinding.FragmentLoginBinding
 import com.example.passwordmanagementapp.util.SignupClick
-import com.example.passwordmanagementapp.viewModel.LoginViewModel
 
 
 class LoginFragment : Fragment(), SignupClick {
 
     private lateinit var dataBinding: FragmentLoginBinding
-    private val viewModel: LoginViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
         return dataBinding.root
     }
